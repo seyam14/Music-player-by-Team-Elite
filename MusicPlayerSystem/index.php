c1ba2253c5a80c0d8fcca81209dac9ee44007547
Seyam <104004121+seyam14@users.noreply.github.com>
2022-11-05 03:13:38 +0600

    Add files via upload

---
+++ MusicPlayerSystem/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Music Player</title>
+    <link rel="icon" type="image/png" href="favicon.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css"/>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+</div>
+<div class="header">  Music Player </div>
+</div> 
+    <div class="player-container">
+       
+        <div class="img-container">
+            <img src="img/image-1.jpg" alt="Album Art">
+        </div>
+        <h2 id="title">Alo</h2>
+        <h3 id="artist">Tahsan</h3>
+        <audio src="music/Alo.mp3"></audio>
+       
+        <div class="progress-container" id="progress-container">
+            <div class="progress" id="progress"></div>
+            <div class="duration-wrapper">
+                <span id="current-time">0:00</span>
+                <span id="duration">4:32</span>
+            </div>
+        </div>
+        <div class="player-controls">
+            <i class="fas fa-backward" id="prev" title="Previous"></i>
+            <i class="fas fa-play main-button" id="play" title="Play"></i>
+            <i class="fas fa-forward" id="next" title="Next"></i>
+        </div>
+    </div>
+    <div class="simp-footer">  @ Project done by Team Elite </div>
+  </div>  
+</div>
+<div class="version"> version: 1.1.1.0</div>
+</div>
+    </div>
+    <script src="js/script.js"></script>
+</body>
+</html>
